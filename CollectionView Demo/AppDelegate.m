bfa4a85c750fae88bf6c5c41ac69ad6c94dd1e7f
Mikkel Selsøe Sørensen <mikkel@shape.dk>
2013-06-14 10:01:42 +0200

    Add basic grid behavior

--- CollectionView Demo/AppDelegate.m
+++ CollectionView Demo/AppDelegate.m
@@ -7,6 +7,8 @@
 //
 
 #import "AppDelegate.h"
+#import "CollectionViewController.h"
+
 
 @implementation AppDelegate
 
@@ -15,6 +17,10 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
+    
+    CollectionViewController *rootViewController = [[CollectionViewController alloc] init];
+    self.window.rootViewController = rootViewController;
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
--- CollectionView Demo/CollectionViewController.m
+++ CollectionView Demo/CollectionViewController.m
@@ -8,31 +8,83 @@
 
 #import "CollectionViewController.h"
 
-@interface CollectionViewController ()
+static NSUInteger const kColorCount = 9;
+static NSString *kCellId = @"CellId";
+
+
+@interface CollectionViewController () {
+    NSArray *__colors;
+    UICollectionViewFlowLayout *__layout;
+}
 
 @end
 
+
+
 @implementation CollectionViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
+- (instancetype)init {
+    
+    if ((self = [super initWithCollectionViewLayout:[self CV_layout]])) {
+        
+        NSMutableArray *colors = [[NSMutableArray alloc] initWithCapacity:kColorCount];
+        for (NSUInteger i = 0; i < kColorCount; i++) {
+            UIColor *color = [[UIColor alloc] initWithRed:arc4random_uniform (255) / 255.0f green:arc4random_uniform (255) / 255.0f blue:arc4random_uniform (255) / 255.0f alpha:1];
+            [colors addObject:color];
+        }
+        __colors = [NSArray arrayWithArray:colors];
+        
+        
     }
+    
     return self;
+    
 }
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:kCellId];
+    self.collectionView.backgroundColor = [UIColor whiteColor];
+    self.collectionView.contentInset = UIEdgeInsetsMake (24, 0, 0, 0);
+    self.collectionView.alwaysBounceVertical = YES;
+    
+    
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [__colors count];
 }
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:kCellId forIndexPath:indexPath];
+    
+    cell.backgroundColor = __colors[[indexPath item]];
+    
+    return cell;
+    
+}
+
+
+
+- (UICollectionViewLayout *)CV_layout {
+    
+    if (!__layout) {
+        
+        __layout = [[UICollectionViewFlowLayout alloc] init];
+        __layout.itemSize = CGSizeMake (100, 100);
+        __layout.scrollDirection = UICollectionViewScrollDirectionVertical;
+        
+        
+    }
+    
+    return __layout;
+}
+
 
 @end
